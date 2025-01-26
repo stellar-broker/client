5a55359375b9947030c2c8231714bb62261244af
orbitlens <orbit.lens@gmail.com>
2025-01-26 01:38:06 -0100

    Update bundling pipeline and restructure dependencies

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,20 +1,3 @@
 module.exports = {
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                corejs: '3.34',
-                useBuiltIns: 'entry',
-                //modules: false,
-                targets: {
-                    browsers: [
-                        '> 1%',
-                        'not dead',
-                        'not op_mini all'
-                    ],
-                    node: '18'
-                }
-            }
-        ]
-    ]
+    presets: [['@babel/preset-env', {targets: {node: 'current'}}]]
 }
